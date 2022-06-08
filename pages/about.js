2f04b559c26c40029efcf826708878364b81ddbf
Jenny Zhang <jenzhang530@gmail.com>
2022-06-07 21:51:12 -0700

    Update about page

--- pages/about.js
+++ pages/about.js
@@ -6,37 +6,67 @@ import TextContainer from "../components/TextContainer";
 import MainContainer from "../components/MainContainer";
 import GlobalStyles from "../components/GlobalStyles";
 import Contact from "../components/Contact";
+import styled from "styled-components";
+
+const ImgFrame = styled.div`
+  width: 150px;
+  height: 150px;
+  position: relative;
+  overflow: hidden;
+  border-radius: 50%;
+  margin-bottom: 30px;
+`;
+
+const ProfileImg = styled.img`
+  display: inline;
+  margin: 0 auto;
+  height: auto;
+  width: 100%;
+`;
+
+const ContentSection = styled.div`
+  margin: 3rem 0;
+  max-width: 500px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const Notule = () => {
   return (
     <BodyContainer>
       <NavBar />
       <MainContainer>
-        <div style={{ margin: "3rem 0" }}>
-          <img width="300px" src="/me.png" />
+        <ContentSection>
+          <ImgFrame>
+            <ProfileImg width="300px" src="/me2022.jpg" />
+          </ImgFrame>
+          <h3>Artist, developer, lazy portfolio updater</h3>
           <p>
-            I'm Jenny, a designer, artist, and Integrated Engineering student at
-            the University of British Columbia in Vancouver, Canada.
             <br />
-            <br />I started drawing at 5 years old and haven't been able to put
-            my pencil down since. I’ve also picked up a lot of new tools and a
-            fascination with technology and engineering along the way.
+            The general gist? I'd say I'm a naturally creative person who picked
+            up a passion for engineering along the way. For me, coding is just
+            another medium of art that I can use to bring my ideas to life. I've
+            been thinking about interactive art installations and would love to
+            design and host one from the ground up. (👀 message me if you're
+            down)
           </p>
+          <br />
           <p>
-            Studying engineering as a designer can feel counterintuitive and
-            often isolating, but I’ve come to appreciate both what I can learn
-            from and provide to either disciplines. Being able to not only
-            imagine, but potentially implement all of my craziest ideas is
-            really exciting!
+            Studying engineering as an artist/designer was... kind of hard!
+            Maybe counterintuitive? Having overcome that hurdle, though, I've
+            come to appreciate both what I can learn from and provide to either
+            disciplines. And the bonus of getting to say I can do both... pretty
+            nice.
           </p>
-          <p></p>
+          <br />
           <p>
-            I like to listen to music under a blue sky in my free time and
-            recently learned Stand Up Paddle-boarding (and have yet to fall in
-            the water)! This is of course, on the rare occasion that I’m not
-            working on some new drawing, photoshoot concept, or design project.
+            Lately I've been trying to cook more, listen to more music, attend
+            more concerts, and spend time with friends. I'll be in NYC this
+            summer for an internship with Lyft and if the person reading this
+            wants to hang out, just say hi!
           </p>
-        </div>
+        </ContentSection>
         <Contact />
       </MainContainer>
       <GlobalStyles />
